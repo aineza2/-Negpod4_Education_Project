9476e31a676f1693c1e87dc0681a6dd397ddeae6
Lydia <ojoawolydia@gmail.com>
2023-11-24 01:21:23 +0100

    work in progress

--- index.py
+++ index.py
@@ -1,16 +1,8 @@
 # Import necessary modules
 import random
+from languages import supported_languages, language_problem_sets
 
-# Define the list of languages supported by the app
-supported_languages = ["Spanish", "French", "German", "Japanese"]
 
-# Define a dictionary to store language problem sets (replace this with actual data)
-language_problem_sets = {
-    "Spanish": {"Hola": "Hello", "Gato": "Cat", "Casa": "House"},
-    "French": {"Bonjour": "Hello", "Chat": "Cat", "Maison": "House"},
-    "German": {"Guten Tag": "Good day", "Katze": "Cat", "Haus": "House"},
-    # Add problem sets for other languages
-}
 
 # Define initial user level
 user_level = 1
---
+++ languages.py
@@ -0,0 +1,12 @@
+# languages.py
+
+# Define the list of languages supported by the app
+supported_languages = ["Spanish", "French", "German", "Japanese"]
+
+# Define a dictionary to store language problem sets (replace this with actual data)
+language_problem_sets = {
+    "Spanish": {"Hola": "Hello", "Gato": "Cat", "Casa": "House"},
+    "French": {"Bonjour": "Hello", "Chat": "Cat", "Maison": "House"},
+    "German": {"Guten Tag": "Good day", "Katze": "Cat", "Haus": "House"},
+    # Add problem sets for other languages
+}

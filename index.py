3f2cae02ea009d7297ad6ebf3e15b080a7c1c61b
Lydia <ojoawolydia@gmail.com>
2023-12-06 01:33:37 +0100

    modification

--- index.py
+++ index.py
@@ -4,32 +4,45 @@ from languages import supported_languages, language_problem_sets
 # Define initial user level
 user_level = 1
 
-# Function to introduce the app and provide rules and guidelines
+def handle_zero_input():
+    while True:
+        print("\nYou pressed 0. Choose an option:")
+        print("1. Return to Main Menu")
+        print("2. Exit the Application")
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            main_menu()
+            break
+        elif choice == "2":
+            print("Exiting the Vocabulary Trainer. Goodbye!")
+            exit()
+        else:
+            print("Invalid choice. Please try again.")
+
 def introduce_app():
-    print(f"Hello! Welcome to the Vocabulary Trainer. You are in Level {user_level}.")
+    print(f"Hello! Welcome to the Vocabulary Trainer.")
     print("Press 0 to see other menu options.")
     menu_choice = input("Your choice: ")
 
     if menu_choice == "0":
-        main_menu()
+        handle_zero_input()
     else:
         print("Invalid choice. Please try again.")
         introduce_app()
 
-# Function to display language learning problems to the user
 def display_problem(language, level):
     problem_set = language_problem_sets[language][level]
     word, translation = random.choice(list(problem_set.items()))
     print(f"Translate the word '{word}' to English.")
-
     return translation
 
-# Function to obtain user input and confirm its correct format
 def get_user_input():
-    user_input = input("Your answer: ").strip().lower()
+    user_input = input("Your answer (press 0 to cancel): ").strip().lower()
+    if user_input == "0":
+        handle_zero_input()
     return user_input
 
-# Function to allow users to choose a language and level
 def choose_language_and_level():
     print("\nChoose a language:")
     for i, language in enumerate(supported_languages, start=1):
@@ -37,7 +50,9 @@ def choose_language_and_level():
 
     language_choice = input("Enter the number of your choice: ")
 
-    if language_choice.isdigit() and 1 <= int(language_choice) <= len(supported_languages):
+    if language_choice == "0":
+        handle_zero_input()
+    elif language_choice.isdigit() and 1 <= int(language_choice) <= len(supported_languages):
         selected_language = supported_languages[int(language_choice) - 1]
         selected_level = choose_level(selected_language)
         return selected_language, selected_level
@@ -45,7 +60,6 @@ def choose_language_and_level():
         print("Invalid choice. Please try again.")
         return choose_language_and_level()
 
-# Function to allow users to choose a level for the selected language
 def choose_level(language):
     levels = list(language_problem_sets[language].keys())
     print(f"\nChoose a level for {language}:")
@@ -54,14 +68,15 @@ def choose_level(language):
 
     level_choice = input("Enter the number of your choice: ")
 
-    if level_choice.isdigit() and 1 <= int(level_choice) <= len(levels):
+    if level_choice == "0":
+        handle_zero_input()
+    elif level_choice.isdigit() and 1 <= int(level_choice) <= len(levels):
         selected_level = levels[int(level_choice) - 1]
         return selected_level
     else:
         print("Invalid choice. Please try again.")
         return choose_level(language)
 
-# Main menu function
 def main_menu():
     print("\nMain Menu:")
     print("1. Learn and Practice")
@@ -72,88 +87,80 @@ def main_menu():
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        # Learn and Practice
         learn_and_practice_menu()
     elif choice == "2":
-        # Rules and Guidelines
         rules_and_guidelines()
     elif choice == "3":
-        # View Level
         print(f"\nYou are currently in Level {user_level}.\n")
         main_menu()
     elif choice == "4":
-        # Exit
-        print("Exiting the Language Vocabulary Trainer. Goodbye!")
+        print("Exiting the Vocabulary Trainer. Goodbye!")
+        exit()
+    elif choice == "0":
+        handle_zero_input()
     else:
         print("Invalid choice. Please try again.")
         main_menu()
 
-# Function to handle Learn and Practice menu
 def learn_and_practice_menu():
     print("\nLearn and Practice Menu:")
     print("1. Pick a Language")
     print("2. Start Practice")
     print("3. Back to Main Menu")
+
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        # Pick a Language
         selected_language, selected_level = choose_language_and_level()
         learn_and_practice(selected_language, selected_level)
-
-    elif choice == "2":
-        # Start Practice (this option should not be here)
-        print("Invalid choice. Please try again.")
-        learn_and_practice_menu()
-
     elif choice == "3":
-        # Back to Main Menu
         main_menu()
-
+    elif choice == "0":
+        handle_zero_input()
     else:
         print("Invalid choice. Please try again.")
         learn_and_practice_menu()
 
-
-
-# Function to learn and practice a specific language
 def learn_and_practice(language, level):
     print(f"\nYou have selected {language} at Level {level}.")
     while True:
-        translation = display_problem(language, level)
+        correct_answer = display_problem(language, level)
         user_input = get_user_input()
 
-        if user_input == "exit":
-            print("Exiting the Language Vocabulary Trainer. Goodbye!")
+        if user_input == "0":
+            continue  # Continue will skip the rest of the loop and start over
+        elif user_input == "exit":
             break
 
-        is_correct = check_answer(user_input, translation)
-        display_result(is_correct)
-        track_user_level(is_correct)
+        is_correct = check_answer(user_input, correct_answer)
+        display_result(is_correct, correct_answer)  # Pass the correct answer for feedback
+        track_user_level(is_correct, correct_answer)  # Pass the correct answer for tracking
 
-# Function to check the correctness of the user's answers
 def check_answer(user_answer, correct_answer):
     return user_answer == correct_answer.lower()
 
-# Function to display the results to the user
-def display_result(is_correct):
+def display_result(is_correct, correct_answer):
     if is_correct:
         print("Correct! Well done.")
     else:
-        print("Incorrect. Keep practicing.")
+        print(f"Incorrect, the correct answer is '{correct_answer}'. Keep practicing.")
 
-# Function to keep track of the user's level in the language learning process
-def track_user_level(is_correct):
+def track_user_level(is_correct, correct_answer):
     global user_level
 
     if is_correct:
         user_level += 1
         print(f"Your level has increased to {user_level}.\n")
     else:
-        user_level = max(1, user_level - 1)
-        print(f"Your level has decreased to {user_level}.\n")
+        user_level -= 1
+        if user_level < 1:
+            print(f"Game over! The correct answer was '{correct_answer}'.")
+            print("Let's start over.")
+            user_level = 1  # Reset level to 1
+            introduce_app()  # Restart the game from the introduction
+        else:
+            print(f"Your level has decreased to {user_level}.\n")
 
-# Function to display rules and guidelines
 def rules_and_guidelines():
     print("\t\tRules and Guidelines:\n")
     print("Welcome to the Vocabulary Trainer! Here are the rules and guidelines:\n")
@@ -163,11 +170,9 @@ def rules_and_guidelines():
     print("4. You can choose a language and practice specific levels.")
     print("5. Each correct answer increases your level, while incorrect answers decrease it.")
     print("6. To exit at any time, type 'exit' as your answer.")
-    print("7. Your progress is saved, and you can continue from where you left off.")
+    print("7. Press 0 at any time to see the exit and main menu options.")
     print("\nLet's start learning! Choose 'Learn and Practice' from the main menu.\n")
-
     main_menu()
 
-# Run the main function when the script is executed
 if __name__ == "__main__":
     introduce_app()
